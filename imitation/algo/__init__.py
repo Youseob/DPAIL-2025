3c0a8f3497e4dd7b914df1225f1f27e7770a1d57
Youseob <37011373+Youseob@users.noreply.github.com>
2025-12-17 14:45:59 +0900

    Add InfoGAIL to algorithm

--- imitation/algo/__init__.py
+++ imitation/algo/__init__.py
@@ -1,7 +1,9 @@
 from .ppo import PPO
 from .gail import GAIL
 from .dpail import DPAIL
+from .infogail import InfoGAIL
 ALGOS = {
     'gail': GAIL,
     'dpail': DPAIL,    
+    'infogail': InfoGAIL
 }
